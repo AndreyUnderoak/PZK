89bd6cb751faf0ce1bcddc93596f31756142390a
Youbot <youbot@eeexeee.com>
2022-07-21 17:21:08 +0300

    MAKE FIRST STEP

---
+++ ozk_pzk.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#length of the YOUBOT's links 
+l = [0.033, 0.147, 0.155, 0.135, 0.2175]
+
+#from angle to radians 
+def to_rad(angle):
+    return angle * np.pi /180
+
+#YOUBOT angle(rad) function
+### time - time in f(t)
+### omega - omega in sin(omega * t)
+### ampl - Amplitude in A * sin(x)
+### joint_num - number of youbot joint
+def r(time, omega, ampl, joint_num):
+    return np.sin(time * omega + np.pi/2) * ampl + thao_angles[joint_num]
+
+#converting from YOUBOT angles(rad) to KINEMATIC model angles(rad)
+def thao(r_array, angle_diff):
+    for i in range(angle_diff.size):
+        r_array[i] = r_array[i] - angle_diff[i]
+    return r_array
+
+#PZK for X
+def x_f(angles):
+    return np.cos(angles[1 -1]) * (l[0] + l[2] * np.sin(angles[2 -1]) + l[3] * np.sin(angles[2 -1] - angles[3-1]) + l[4] * np.sin(angles[2 -1] - angles[3-1] + angles[4 -1]))
+
+#PZK for Y
+def y_f(angles):
+    return - np.sin(angles[1 -1]) * (l[0] + l[2] * np.sin(angles[2 -1]) + l[3] * np.sin(angles[2 -1] - angles[3-1]) + l[4] * np.sin(angles[2 -1] - angles[3-1] + angles[4 -1]))
+
+#PZK for Z
+def z_f(angles):
+    return (l[1] + l[2] * np.cos(angles[2 -1]) + l[3] * np.cos(angles[2 -1] - angles[3-1]) + l[4] * np.cos(angles[2 -1] - angles[3-1] + angles[4 -1]))
+
+
+#middle angles of YOUBOT
+thao_angles = np.array([169, 65, -146, 102, 167])
+thao_angles = to_rad(thao_angles)
+
+#creating time in the UNIVERSE
+time = np.arange(0, 200, 0.2)
+
+#array of their values in time by R function
+r_array = [r(time, 1.8, np.pi/4, 1 -1),
+r(time, 1.8, np.pi/4, 2 -1),
+r(time, 1.8, np.pi/4, 3 -1),
+r(time, 1.8, np.pi/4, 4 -1)]
+
+#converting to KINEMATIC model
+thao_array = thao(r_array, thao_angles)
+
+#PZK coordinates
+x = x_f(thao_array)
+y = y_f(thao_array)
+z = z_f(thao_array)
+
+#GRAPH
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.scatter(x, y, z)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
