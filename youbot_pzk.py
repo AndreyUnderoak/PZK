c703ab863ea8101a95e59a1cf4b152d8ebaea794
Your Name <you@example.com>
2022-08-22 13:17:56 +0300

    OZK for youbot half-ready

--- youbot_pzk.py
+++ youbot_pzk.py
@@ -11,7 +11,8 @@ class Youbot :
     youbot_angles = np.radians(np.array([169, 65, -146, 102, 167]))
 
     class Link:
-        def __init__(self, offset, d, a, alpha):
+        def __init__(self, flip, offset, d, a, alpha):
+            self.flip = flip
             self.offset = offset
             self.d      = d
             self.a      = a
@@ -26,11 +27,11 @@ class Youbot :
     # ])
 
     links = np.array([
-        Link(offset = 0,        d = links_length[1],   a = links_length[0], alpha = np.pi/2),
-        Link(offset = -np.pi/2, d = 0,                 a = links_length[2], alpha = -np.pi),
-        Link(offset = 0,        d = 0,                 a = links_length[3], alpha = np.pi),
-        Link(offset = -np.pi/2, d = 0,                 a = 0,               alpha = np.pi/2),
-        Link(offset = 0,        d = -links_length[4],  a = 0,               alpha = 0)
+        Link(flip = True,  offset = 0,        d = links_length[1],   a = links_length[0], alpha = -np.pi/2),
+        Link(flip = False, offset = -np.pi/2, d = 0,                 a = links_length[2], alpha = -np.pi),
+        Link(flip = False, offset = 0,        d = 0,                 a = links_length[3], alpha = np.pi),
+        Link(flip = False, offset = -np.pi/2, d = 0,                 a = 0,               alpha = np.pi/2),
+        Link(flip = False, offset = 0,        d = -links_length[4],  a = 0,               alpha = 0)
     ])
 
     def __init__(self):
@@ -39,9 +40,9 @@ class Youbot :
     def view_model_init(self, links):
         temp = np.zeros(len(links), rtb.RevoluteDH)
         for i in range(len(links)):
-            temp[i] = rtb.RevoluteDH(d=links[i].d, a=links[i].a, alpha=links[i].alpha, offset = links[i].offset)
+            temp[i] = rtb.RevoluteDH(d=links[i].d, a=links[i].a, alpha=links[i].alpha, offset = links[i].offset, flip=links[i].flip)
         
-        self.view_model = rtb.DHRobot(temp.tolist(), base = spm.SE3.Rx(np.pi), name="YOUBOT")
+        self.view_model = rtb.DHRobot(temp.tolist(), name="YOUBOT")
         #self.view_model.plot(self.view_model.q, block=True)
     
     #converting from YOUBOT angles(rad) to KINEMATIC model angles(rad)
@@ -65,7 +66,7 @@ class Youbot :
                            [0                                                          , 0                                                                                                 , 0                                                                                                 , 1                                                                                 ]])
 
     def h_matrix(self, theta_array, start, finish):
-        a = self.a_matrix(theta_array, start)
+        a = self.a_matrix( - theta_array, start)
 
         for i in range(start + 1, finish):
             a = np.dot(a , self.a_matrix(theta_array, i))
@@ -100,7 +101,7 @@ class Youbot :
         t_1_temp = self.t_1(x, y, conf_t_1)
         xp = x * np.cos(t_1_temp) + y * np.sin(t_1_temp)
 
-        p_square = (xp - l[0])**2 + z**2
+        p_square = (xp - l[0])**2 + (z-l[1])**2
         
         cosT_3  = - (l[2]**2 + l[3]**2 - p_square) / (2 * l[2] * l[3])
 
@@ -121,14 +122,14 @@ class Youbot :
         t_3_temp = self.t_3(x, y, z, l, conf_t_1, conf_t_3)
         beta = np.arctan2(l[3]*np.sin(t_3_temp), l[2]+l[3]*np.cos(t_3_temp))
 
-        return np.arctan2(xp - l[0], z) - beta
+        return np.arctan2(xp - l[0], z - l[1]) - beta
 
     def inverse_position(self, coordinates, conf_t_1, conf_t_3):
 
         return np.array([
-                    self.t_1(coordinates[0], coordinates[1], conf_t_1),
+                    - self.t_1(coordinates[0], coordinates[1], conf_t_1),
                     self.t_2(coordinates[0], coordinates[1], coordinates[2], self.links_length, conf_t_1, conf_t_3),
-                    self.t_3(coordinates[0], coordinates[1], coordinates[2], self.links_length, conf_t_1, conf_t_3)
+                    - self.t_3(coordinates[0], coordinates[1], coordinates[2], self.links_length, conf_t_1, conf_t_3)
                 ])
 
     def inverse_orientation(self, theta_array):
@@ -147,6 +148,9 @@ class Youbot :
     def inverse_get_theta_array(self, coordinates, r05, conf_t_1, conf_t_3):
         #shift to p coordinates
         p = coordinates - np.dot(r05, np.array([0, 0, -self.links_length[4]]))
+
+        print("p=", p)
+
         theta_array = self.inverse_position(p, conf_t_1, conf_t_3)
 
         print(theta_array)
@@ -184,7 +188,8 @@ r05 = youbot.get_rot_matrix(theta_array, 0, 5)
 
 print(r05)
 
-youbot.view_model.plot(theta_array, block=True)
+#youbot.view_model.plot(theta_array, block=True)
+youbot.view_model.teach()
 plt.show()
 # r05[1][1] = 1
 # r05[0][0] = r05[2][2] = np.cos(angle_const)
@@ -195,19 +200,21 @@ plt.show()
 x = 0.3
 y = 0
 z = 0.2
-coordinates = youbot.get_end_effector_coors(theta_array)
+coordinates = np.array([x,y,z])
 
 print(coordinates)
 
-theta_array = youbot.inverse_get_theta_array(coordinates, r05, conf_t_1 = 1, conf_t_3 = 1)
+theta_array = youbot.inverse_get_theta_array(coordinates, r05, conf_t_1 = 2, conf_t_3 = 1)
 
 
 
 print(theta_array)
 
 
-#print(youbot_dh.get_end_effector_coors(theta_array))
+print(youbot.get_end_effector_coors(theta_array))
 
+youbot.view_model.plot(theta_array, block=True)
+plt.show()
 # h = youbot_dh.view_model.fkine([theta_1, theta_2, theta_3, theta_4, theta_5])
 # youbot_dh.view_model.plot([theta_1, theta_2, theta_3, theta_4, theta_5], block=True)
 #plt.show()
